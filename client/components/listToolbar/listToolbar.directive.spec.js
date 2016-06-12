ded0c9b259ec2d4e1f656e8050fdad34e936454f
Wyatt Lindsey <wyatt@newrelic.com>
2016-06-11 18:17:17 -0700

    fix lvl-drag-drop deployment issues

--- client/components/listToolbar/listToolbar.directive.spec.js
+++ client/components/listToolbar/listToolbar.directive.spec.js
@@ -6,11 +6,32 @@ describe('Directive: listToolbar', function () {
   beforeEach(angular.mock.module('prindleApp'));
   beforeEach(angular.mock.module('components/listToolbar/listToolbar.html'));
 
-  var element, scope;
+  var $compile,
+    $rootScope,
+    $controller;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
+  beforeEach(inject(function (_$compile_, _$rootScope_, _$controller_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    $controller = _$controller_;
   }));
+
+//  beforeEach(inject(function (_listToolbar_) {
+//    listToolbar = _listToolbar_;
+//  }));
+
+  it('should work', function() {
+    expect(1).to.equal(1);
+  });
+
+//  it('should initialize correctly', function() {
+//    var element = $compile()
+//  });
+
+  // this would go in the child tests
+//  it('places the correct elements in the toolbar', function() {
+//    var listToolbar = $compile()($rootScope);
+//  });
 
 //  it('should make hidden element visible', inject(function ($compile) {
 //    element = angular.element('<list-toolbar></list-toolbar>');
